6b9408568ea3c2cba8abda3b7b5077b8f5af0594
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-07-04 19:01:14 +0530

    complete UI

--- src/components/ColorChanger/app.jsx
+++ src/components/ColorChanger/app.jsx
@@ -15,10 +15,12 @@ function ColorChanger({ isDark, setIsDark, checkedColor }) {
           onChange={toggleSwitch}
         />
         <div
-          className={`w-11 h-6  peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500 rounded-full peer dark:bg-gray-600 
-                      peer-checked:after:translate-x-full peer-checked:after:border-white 
-                      after:content-[''] after:absolute after:top-0.5 after:left-[2px] ${checkedColor} after:border-gray-300 
-                      after:border after:rounded-full after:h-5 after:w-5 after:transition-all`}
+          className={`w-11 h-6 border-2 border-black peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-500  rounded-full peer dark:bg-gray-800 
+  peer-checked:after:translate-x-full peer-checked:after:border-white 
+  after:content-[''] after:absolute after:top-0.5 after:left-[2px] 
+  ${checkedColor} 
+  after:border-gray-300 after:border after:rounded-full 
+  after:h-5 after:w-5 after:transition-all`}
         ></div>
       </label>
     </div>
